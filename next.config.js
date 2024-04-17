cf22081f86ac887ec51e31961c3158131783a9d9
Arpan Goel <arpangoel@Arpans-MacBook-Air.local>
2024-04-17 17:29:55 +0530

    first-commit

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
         hostname: "img.clerk.com",
       },
     ],
+    
   },
 };
 
--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -1,6 +1,7 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 
 const apiKey = process.env.PINECONE_API_KEY;
+console.log(apiKey);
 
 if (!apiKey) {
   throw Error("PINECONE_API_KEY is not set");
@@ -10,5 +11,8 @@ const pinecone = new Pinecone({
   environment: "gcp-starter",
   apiKey,
 });
+console.log(pinecone);
+export const notesIndex = pinecone.Index("note-app");
 
-export const notesIndex = pinecone.Index("nextjs-ai-note-app");
+
+//https://aiapp-w7pp2ss.svc.aped-4627-b74a.pinecone.io
